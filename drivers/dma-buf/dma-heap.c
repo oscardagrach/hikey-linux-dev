8487e4be78b928f7a22d79273d945fd027f6d4dd
John Stultz <john.stultz@linaro.org>
2019-10-02 03:12:09 +0000

    dma-buf: heaps: Allow system & cma heaps to be configured as a modules
    Allow loading system and cma heap as a module instead of just as
    a statically built in heap.
    
    Since there isn't a good mechanism for dmabuf lifetime tracking
    it isn't safe to allow the heap drivers to be unloaded, so these
    drivers do not implement any module unloading functionality and
    will show up in lsmod as "[permanent]".
    
    This patch also exports key functions from dmabuf heaps core and
    the heap helper functions so they can be accessed by the module.
    
    Cc: Laura Abbott <labbott@redhat.com>
    Cc: Benjamin Gaignard <benjamin.gaignard@linaro.org>
    Cc: Sumit Semwal <sumit.semwal@linaro.org>
    Cc: Liam Mark <lmark@codeaurora.org>
    Cc: Pratik Patel <pratikp@codeaurora.org>
    Cc: Brian Starkey <Brian.Starkey@arm.com>
    Cc: Andrew F. Davis <afd@ti.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Yue Hu <huyue2@yulong.com>
    Cc: Mike Rapoport <rppt@linux.ibm.com>
    Cc: Chenbo Feng <fengc@google.com>
    Cc: Alistair Strachan <astrachan@google.com>
    Cc: Sandeep Patil <sspatil@google.com>
    Cc: Hridya Valsaraju <hridya@google.com>
    Cc: dri-devel@lists.freedesktop.org
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- drivers/dma-buf/dma-heap.c
+++ drivers/dma-buf/dma-heap.c
@@ -191,6 +191,7 @@ void *dma_heap_get_drvdata(struct dma_heap *heap)
 {
 	return heap->priv;
 }
+EXPORT_SYMBOL_GPL(dma_heap_get_drvdata);
 
 static void dma_heap_release(struct kref *ref)
 {
@@ -295,6 +296,7 @@ err0:
 	kfree(heap);
 	return err_ret;
 }
+EXPORT_SYMBOL_GPL(dma_heap_add);
 
 static char *dma_heap_devnode(struct device *dev, umode_t *mode)
 {
